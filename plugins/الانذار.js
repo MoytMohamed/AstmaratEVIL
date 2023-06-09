21b5574a5f7754b9763daaabb62a2f95ccaf641a
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-09 09:11:31 +0300

    Create الانذار.js

---
+++ plugins/الانذار.js
@@ -0,0 +1,47 @@
+let handler = async (m, { conn}) => {
+
+let user = global.db.data.users[m.sender]
+
+let name = conn.getName(m.sender)
+
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+
+let username = conn.getName(who)
+
+let av = `./src/mp3/${pickRandom([""])}.mp3`
+
+m.reply( `❆═━━━━═⌠🔥⌡═━━━━═❆
+
+*【🕊️بطاقة┃⛔┃إنـــذار🕊️】* 
+
+❆═━━━━═⌠🔥⌡═━━━━═❆
+
+*الـلـقـب 🎎 ⎢*  『 』
+
+*الـسـبـب ❓⎢*  『 』
+
+*عـدد الانـذارات 🚨 ⎢*  『 』
+
+❆═━━━━═⌠🔥⌡═━━━━═❆
+
+✠ تــ✍︎ــوقــيــع ↯:
+
+【 𝐄 𝐕 𓆩🔥𓆪 𝐈 𝐋 】`)
+
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+
+} 
+
+handler.customPrefix = /^(انذار)$/i
+
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+
+  return list[Math.floor(list.length * Math.random())]
+
+}
