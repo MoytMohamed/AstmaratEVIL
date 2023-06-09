68699fe30813a0fb1c182d2c062ef91c0246e34f
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-09 09:25:07 +0300

    Create ترقيه.js

---
+++ plugins/ترقيه.js
@@ -0,0 +1,49 @@
+let handler = async (m, { conn}) => {
+
+let user = global.db.data.users[m.sender]
+
+let name = conn.getName(m.sender)
+
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+
+let username = conn.getName(who)
+
+let av = `./src/mp3/${pickRandom([""])}.mp3`
+
+m.reply( `❆═━━━━═⌠🔥⌡═━━━━═❆
+
+اعـلان تـرقـيـة 🎐
+
+❆═━━━━═⌠🔥⌡═━━━━═❆
+
+*مـن 👨🏻‍💼 ⎢* 〖 〗
+
+*الـى 👮🏻‍♂️ ⎢* 〖 〗
+
+❆═━━━━═⌠🔥⌡═━━━━═❆
+
+○ نـتـمـنـى مـنه ايـداء واجـبـه بـكـل اخـلاص، بـالـتـوفـيـق 🤍.
+
+❆═━━━━═⌠🔥⌡═━━━━═❆
+
+✠ تــ✍︎ــوقــيــع ↯:
+
+【 𝐄 𝐕 𓆩🔥𓆪 𝐈 𝐋 】`)
+
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+
+} 
+
+handler.customPrefix = /^(ترقية)$/i
+
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+
+  return list[Math.floor(list.length * Math.random())]
+
+}
