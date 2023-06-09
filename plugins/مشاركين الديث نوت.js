271ac0454375eff46dc9816e28fceceb9f01c2ec
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-09 09:42:46 +0300

    Create مشاركين الديث نوت.js

---
+++ plugins/مشاركين الديث نوت.js
@@ -0,0 +1,53 @@
+let handler = async (m, { conn}) => {
+
+let user = global.db.data.users[m.sender]
+
+let name = conn.getName(m.sender)
+
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+
+let username = conn.getName(who)
+
+let av = `./src/mp3/${pickRandom([""])}.mp3`
+
+m.reply( `*فعالية الديـث نـوت 📓*
+
+〖 اللقب 〗 ¦ 〖 النقاط 〗 
+
+*↤「」¦「」*📓
+
+*↤「」¦「」*📓
+
+*↤「」¦「」*📓
+
+*↤「」¦「」*📓 
+
+*↤「」¦「」*📓
+
+*↤「」¦「」*📓
+
+*↤「」¦「」*📓
+
+*↤「」¦「」*📓
+
+*↤「」¦「」*📓
+
+*↤「」¦「」*📓`)
+
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+
+} 
+
+handler.customPrefix = /^(مشاركين الديث نوت)$/i
+
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+
+  return list[Math.floor(list.length * Math.random())]
+
+}
