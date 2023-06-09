3c45a404c0112ffe79536bb8fd3f01b27a965a65
Gojollp <126991725+Gojollp@users.noreply.github.com>
2023-06-09 09:20:08 +0300

    Create النتائج.js

---
+++ plugins/النتائج.js
@@ -0,0 +1,71 @@
+let handler = async (m, { conn}) => {
+
+let user = global.db.data.users[m.sender]
+
+let name = conn.getName(m.sender)
+
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+
+let username = conn.getName(who)
+
+let av = `./src/mp3/${pickRandom([""])}.mp3`
+
+m.reply( `❆═━━━━═⌠🔥⌡═━━━━═❆
+
+     *╎🔥 نتائج الفعالية 🔥╎*
+
+❆═━━━━═⌠🔥⌡═━━━━═❆
+
+*❍ الـمُـقَـدِّمْ🎙️╎*『』
+
+*❍ الـجـائـزة 💰 ╎*『』
+
+*❏ المركز الأول🥇*『』
+
+ • الجائزة💰╎
+
+*❏ المركز الثاني🥈*『』
+
+• الجائزة💰╎
+
+*❏ المركز الثالث🥉*『』
+
+• الجائزة💰╎
+
+*‏❅ ━ ── • 🔥 • ── ━ ❅*
+
+⟐ بقية المشاركين 👥 :
+
+• الجائزة💰╎10k
+
+‏- 『』
+
+‏- 『』
+
+‏- 『』
+
+‏- 『』
+
+❆═━━━━═⌠🔥⌡═━━━━═❆
+
+✠ تــ✍︎ــوقــيــع ↯:
+
+【 𝐄 𝐕 𓆩🔥𓆪 𝐈 𝐋 】`)
+
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+
+} 
+
+handler.customPrefix = /^(النتائج)$/i
+
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+
+  return list[Math.floor(list.length * Math.random())]
+
+}
